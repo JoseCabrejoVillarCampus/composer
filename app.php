562909406934aff9fd97076e424c95d8439eed24
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-24 21:10:43 -0500

    feat: :sparkles: Cracion rutas campers
    creamos las rutas para tb campers

--- app.php
+++ app.php
@@ -26,19 +26,62 @@
     ?rutas  admin_area
     */
     $router->get('/api/admin_area', function(){
-        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAcademicArea();
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAdminArea();
     });
     $router->post('/api/admin_area/post', function(){
-        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->postOptionalTopics();
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->postAdminArea();
     });
     $router->post('/api/admin_area/put', function(){
-        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->putOptionalTopics();
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->putAdminArea();
     });
     $router->post('/api/admin_area/del', function(){
-        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->deleteOptionalTopics();
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->deleteAdminArea();
     });
-    
-    
+    /*
+    ?rutas  areas
+    */
+    $router->get('/api/areas', function(){
+        \App\areas\areas::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAreas();
+    });
+    $router->post('/api/areas/post', function(){
+        \App\areas\areas::getInstance(json_decode(file_get_contents("php://input"),true))->postAreas();
+    });
+    $router->post('/api/areas/put', function(){
+        \App\areas\areas::getInstance(json_decode(file_get_contents("php://input"),true))->putAdminArea();
+    });
+    $router->post('/api/areas/del', function(){
+        \App\areas\areas::getInstance(json_decode(file_get_contents("php://input"),true))->deleteAreas();
+    });
+    /*
+    ?rutas  campers
+    */
+    $router->get('/api/campers', function(){
+        \App\campers\campers::getInstance(json_decode(file_get_contents("php://input"),true))->getAllCamper();
+    });
+    $router->post('/api/campers/post', function(){
+        \App\campers\campers::getInstance(json_decode(file_get_contents("php://input"),true))->postCamper();
+    });
+    $router->post('/api/campers/put', function(){
+        \App\campers\campers::getInstance(json_decode(file_get_contents("php://input"),true))->putCamper();
+    });
+    $router->post('/api/campers/del', function(){
+        \App\campers\campers::getInstance(json_decode(file_get_contents("php://input"),true))->deleteCamper();
+    });
+    /*
+    ?rutas  chapters
+    */
+    $router->get('/api/chapters', function(){
+        \App\chapters\chapters::getInstance(json_decode(file_get_contents("php://input"),true))->getAllChapters();
+    });
+    $router->post('/api/chapters/post', function(){
+        \App\chapters\chapters::getInstance(json_decode(file_get_contents("php://input"),true))->postChapters();
+    });
+    $router->post('/api/chapters/put', function(){
+        \App\chapters\chapters::getInstance(json_decode(file_get_contents("php://input"),true))->putChapters();
+    });
+    $router->post('/api/chapters/del', function(){
+        \App\chapters\chapters::getInstance(json_decode(file_get_contents("php://input"),true))->deleteChapters();
+    });
     $router->run();
 
 ?>
