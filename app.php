d9cac906bc56bbc541ad2688e9f24ce79173b205
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-24 21:04:33 -0500

    feat: :sparkles: Rutas academic_area
    creacion de las rutas para academic area

--- app.php
+++ app.php
@@ -8,16 +8,19 @@
 
     
 
-    $router->get('/api', function(){
+    $router->get('/api/academic_area', function(){
 
-        \App\optional_topics\optional_topics::getInstance(json_decode(file_get_contents("php://input"),true))->getAllOptionalTopics();
-
+        \App\academic_area\academic_area::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAcademicArea();
     });
-
-    $router->get('/api/otra', function(){
-        echo "perras todas";
+    $router->post('/api/academic_area/post', function(){
+        \App\optional_topics\optional_topics::getInstance(json_decode(file_get_contents("php://input"),true))->postOptionalTopics();
     });
-
+    $router->post('/api/academic_area/put', function(){
+        \App\optional_topics\optional_topics::getInstance(json_decode(file_get_contents("php://input"),true))->putOptionalTopics();
+    });
+    $router->post('/api/academic_area/del', function(){
+        \App\optional_topics\optional_topics::getInstance(json_decode(file_get_contents("php://input"),true))->deleteOptionalTopics();
+    });
     $router->run();
 
 ?>
