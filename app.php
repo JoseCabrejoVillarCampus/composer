e60ca95f8dc4dd85cb4a5dfdc34067ca1331f192
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-06-24 21:06:53 -0500

    feat: :sparkles: Rutas admin_area
    Creacion de las rutas para admin_area

--- app.php
+++ app.php
@@ -7,9 +7,10 @@
     $router = new \Bramus\Router\Router();
 
     
-
+    /* 
+    ? rutas academic_area
+    */
     $router->get('/api/academic_area', function(){
-
         \App\academic_area\academic_area::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAcademicArea();
     });
     $router->post('/api/academic_area/post', function(){
@@ -21,6 +22,23 @@
     $router->post('/api/academic_area/del', function(){
         \App\optional_topics\optional_topics::getInstance(json_decode(file_get_contents("php://input"),true))->deleteOptionalTopics();
     });
+    /*
+    ?rutas  admin_area
+    */
+    $router->get('/api/admin_area', function(){
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->getAllAcademicArea();
+    });
+    $router->post('/api/admin_area/post', function(){
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->postOptionalTopics();
+    });
+    $router->post('/api/admin_area/put', function(){
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->putOptionalTopics();
+    });
+    $router->post('/api/admin_area/del', function(){
+        \App\admin_area\admin_area::getInstance(json_decode(file_get_contents("php://input"),true))->deleteOptionalTopics();
+    });
+    
+    
     $router->run();
 
 ?>
